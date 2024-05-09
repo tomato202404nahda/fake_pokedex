ef035f569de5d78a079a19b555b3b80d457fc364
laroiba <tomato.202404.nahda@gmail.com>
2024-05-09 16:38:51 +0900

    third commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <div className="flex flex-row justify-between p-4 items-center bg-cyan-600 text-slate-200 rounded-b-lg">
-      <h1 className="text-2xl font-bold">Fake Pokedex</h1>
+      <a href="/">
+        <h1 className="text-2xl font-bold">Fake Pokedex</h1>
+      </a>
       <p>React tutorial</p>
     </div>
   );
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -75,11 +75,67 @@ export const Main = () => {
 
       {
         <Card title={pokemonName}>
-          <img
-            className="max-w-[50vw] md:max-w-[20vw] w-full h-auto mx-auto"
-            src={pokemonImage}
-            alt=""
-          />
+          <div className="flex flex-row items-center">
+            <button
+              type="button"
+              onClick={() => {
+                setNumber((p) => {
+                  if (p === 1) {
+                    return p;
+                  }
+                  return --p;
+                });
+              }}
+              className="bg-gray-800 text-white rounded-l-md border-r max-h-[10%] max-w-auto border-gray-100 py-2 hover:bg-red-700 hover:text-white px-3"
+            >
+              <div className="flex flex-row align-middle">
+                <svg
+                  className="w-5 mr-2"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </button>
+            <img
+              className="max-w-[50vw] md:max-w-[20vw] w-full h-auto mx-auto"
+              src={pokemonImage}
+              alt=""
+            />
+            <button
+              type="button"
+              onClick={() => {
+                setNumber((p) => {
+                  if (p === 1025) {
+                    return p;
+                  }
+                  return ++p;
+                });
+              }}
+              className="bg-gray-800 text-white rounded-r-md py-2 border-l border-gray-200 hover:bg-red-700 hover:text-white px-3"
+            >
+              <div className="flex flex-row align-middle">
+                <svg
+                  className="w-5 ml-2"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </button>
+          </div>
           <div className="flex flex-col p-4 gap-6 text-center">
             <div>
               <h3>Pokemon ID</h3>
