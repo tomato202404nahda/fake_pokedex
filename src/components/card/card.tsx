b1127811a7b671ba14fd657a062218959ca006b2
laroiba <tomato.202404.nahda@gmail.com>
2024-05-09 16:28:55 +0900

    2nd commit

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -5,8 +5,8 @@ type Props = {
 
 export default function Card(props: Props) {
   return (
-    <div className="max-w-full md:max-w-[20vw] max-h-[30vh] w-full box-border overflow-y-auto mx-auto rounded-lg border shadow-md shadow-black border-blue-500">
-      <div className="bg-black py-4 text-white rounded-t-lg ">
+    <div className="max-w-full md:max-w-[20vw]  w-full box-border mx-auto rounded-lg border shadow-md shadow-black border-blue-500">
+      <div className="bg-black py-4  text-white rounded-t-lg ">
         <h2 className="text-xl text-center">{props.title.toUpperCase()}</h2>
       </div>
       {props.children}
--- src/pages/details/Details.tsx
+++ src/pages/details/Details.tsx
@@ -18,24 +18,36 @@ export default function Details() {
           src={pokemon.sprites.front_default}
           alt=""
         />
-        <div className="flex flex-row gap-6">
+        <Card title="Basic Information">
+          <div className="border rounded grow flex flex-row px-8 justify-between">
+            <p>Height</p>
+            <p>{pokemon.height / 10}m</p>
+          </div>
+          <div className="border rounded grow flex flex-row px-8 justify-between">
+            <p>Weight</p>
+            <p>{pokemon.weight / 10}kg</p>
+          </div>
+        </Card>
+        <div className="flex flex-row gap-6 flex-wrap">
           <Card title="Abilities">
             {Object.keys(pokemon.abilities).map((key) => {
               return (
-                <div className="border rounded">
+                <div className="border rounded grow">
                   {pokemon.abilities[key].ability.name.toUpperCase()}
                 </div>
               );
             })}
           </Card>
           <Card title="Moves">
-            {Object.keys(pokemon.moves).map((key) => {
-              return (
-                <div className="border rounded">
-                  {pokemon.moves[key].move.name.toUpperCase()}
-                </div>
-              );
-            })}
+            <div className="max-h-[30vh] box-content overflow-y-auto">
+              {Object.keys(pokemon.moves).map((key) => {
+                return (
+                  <div className="border rounded">
+                    {pokemon.moves[key].move.name.toUpperCase()}
+                  </div>
+                );
+              })}
+            </div>
           </Card>
         </div>
       </div>
